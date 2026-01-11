3845fd09de1432f30b09014faab882bea1581af5
Ben McMillen <benmcmillen12@gmail.com>
2026-01-11 16:57:56 +1100

    consistent white colour scheme

--- .aws-sam/build/ApiFunction/main.py
+++ .aws-sam/build/ApiFunction/main.py
@@ -12,7 +12,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",  # Vite
         "http://localhost:3000",  # CRA / Next
-        "https://cover-letter-builder-evch.vercel.app/"
+        "https://cover-letter-builder-qpml.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
